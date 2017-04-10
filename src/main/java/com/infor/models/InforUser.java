c6991c1805055d8c7511d56132b60608dfc09a83
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-04-10 16:13:51 +0800

    add models for the project

--- src/main/java/com/infor/models/InforUser.java
+++ src/main/java/com/infor/models/InforUser.java
@@ -1,5 +1,117 @@
 package com.infor.models;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_inforuser")
 public class InforUser {
 	public InforUser(){}
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "userid")
+	private int userid;
+	
+	@Column(name = "firstname")
+	private String firstname;
+	
+	@Column(name = "lastname")
+	private String lastname;
+	
+	@Column(name = "contactnumber")
+	private String contactnumber;
+	
+	@Column(name = "emailaddress")
+	private String emailaddress;
+	
+	@Column(name = "inforaddress")
+	private String inforaddress;
+	
+	@Column(name = "position")
+	private String position;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	public int getUserid() {
+		return userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getContactnumber() {
+		return contactnumber;
+	}
+
+	public void setContactnumber(String contactnumber) {
+		this.contactnumber = contactnumber;
+	}
+
+	public String getEmailaddress() {
+		return emailaddress;
+	}
+
+	public void setEmailaddress(String emailaddress) {
+		this.emailaddress = emailaddress;
+	}
+
+	public String getInforaddress() {
+		return inforaddress;
+	}
+
+	public void setInforaddress(String inforaddress) {
+		this.inforaddress = inforaddress;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
 }
