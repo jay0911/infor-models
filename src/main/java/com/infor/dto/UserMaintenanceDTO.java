ad5996602129e225e45d458f401f15d64279d6d2
jaypogi69 <jayoliveros286@gmail.com>
2017-04-12 02:14:58 +0800

    add roles object to dto

--- src/main/java/com/infor/dto/UserMaintenanceDTO.java
+++ src/main/java/com/infor/dto/UserMaintenanceDTO.java
@@ -1,5 +1,19 @@
 package com.infor.dto;
 
+import java.util.List;
+
+import com.infor.models.InforRoles;
+
 public class UserMaintenanceDTO {
 	public UserMaintenanceDTO(){}
+	
+	private List<InforRoles> inforRoles;
+	
+	public List<InforRoles> getInforRoles() {
+		return inforRoles;
+	}
+
+	public void setInforRoles(List<InforRoles> inforRoles) {
+		this.inforRoles = inforRoles;
+	} 
 }
