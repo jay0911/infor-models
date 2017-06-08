34daa9f9ef7c39aea9c875c8074f697644ef352a
jaypogi69 <jayoliveros286@gmail.com>
2017-06-08 23:47:43 +0800

    add car information to dto transaction

--- src/main/java/com/infor/dto/TransactionDTO.java
+++ src/main/java/com/infor/dto/TransactionDTO.java
@@ -1,11 +1,15 @@
 package com.infor.dto;
 
+import java.util.List;
+
 import com.infor.models.AjaxResponseBody;
+import com.infor.models.InforCar;
 import com.infor.models.InforParking;
 import com.infor.models.InforUser;
 
 public class TransactionDTO {
 	private InforParking inforParking;
+	private List<InforCar> inforCars;
 	private InforUser tandemParkingDetails;
 	private AjaxResponseBody ajaxResponseBody;
 	public InforParking getInforParking() {
@@ -25,5 +29,11 @@ public class TransactionDTO {
 	}
 	public void setTandemParkingDetails(InforUser tandemParkingDetails) {
 		this.tandemParkingDetails = tandemParkingDetails;
+	}
+	public List<InforCar> getInforCars() {
+		return inforCars;
+	}
+	public void setInforCars(List<InforCar> inforCars) {
+		this.inforCars = inforCars;
 	}
 }
