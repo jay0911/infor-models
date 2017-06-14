59db53684f5c29647ee23b9d10138511b6fb97ba
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-06-14 14:22:58 +0800

    add emailer

--- src/main/java/com/infor/models/Email.java
+++ src/main/java/com/infor/models/Email.java
@@ -1,5 +1,15 @@
 package com.infor.models;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class Email {
 	private String host;
 	private String socketPort;
@@ -12,6 +22,21 @@ public class Email {
 	private String toAddress;
 	private String subject;
 	private String message;
+	private Message msg;
+	private Properties props;
+	
+	public Email(){}
+	
+	public Email(String host,String socketPort,String socketClass,String auth,String port,String username,String password){
+		this.host = host;
+		this.socketPort = socketPort;
+		this.socketClass = socketClass;
+		this.auth = auth;
+		this.port = port;
+		this.userName = username;
+		this.password = password;
+		setProps();
+	}
 	
 	public String getHost() {
 		return host;
@@ -79,6 +104,56 @@ public class Email {
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
+	public Message getMsg() {
+		return msg;
+	}
+
+	public void setMsg(Message msg) {
+		this.msg = msg;
+	}
+
+	public Properties getProps() {
+		return props;
+	}
+
+	public void setProps(Properties props) {
+		this.props = props;
+	}
 	
+	private void setProps(){
+		props = new Properties();
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.socketFactory.port", socketPort);
+		props.put("mail.smtp.socketFactory.class", socketClass);
+		props.put("mail.smtp.auth", auth);
+		props.put("mail.smtp.port", port);
+	}
+	
+	public void send(){
+		setProps();
+		
+		Session session= Session.getDefaultInstance(getProps(),
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(getUserName(),getPassword());
+					}
+				});
+		try {
+			msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(getSenderAddress()));
+			msg.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(getToAddress()));
+			msg.setSubject(getSubject());
+			msg.setText(getMessage());
+
+			Transport.send(msg);
+
+			System.out.println("Done sending email");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 	
 }
