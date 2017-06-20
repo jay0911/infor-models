cef3cd44a7da122e9102672efa30eac2b703639c
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-06-20 17:47:59 +0800

    baseline for slot module

--- src/main/java/com/infor/dto/SlotsDTO.java
+++ src/main/java/com/infor/dto/SlotsDTO.java
@@ -1,5 +1,19 @@
 package com.infor.dto;
 
+import java.util.List;
+
+import com.infor.models.InforSlots;
+
 public class SlotsDTO {
 	public SlotsDTO(){}
+	
+	public List<InforSlots> getInforSlots() {
+		return inforSlots;
+	}
+
+	public void setInforSlots(List<InforSlots> inforSlots) {
+		this.inforSlots = inforSlots;
+	}
+
+	private List<InforSlots> inforSlots;
 }
--- src/main/java/com/infor/models/InforSlots.java
+++ src/main/java/com/infor/models/InforSlots.java
@@ -1,5 +1,19 @@
 package com.infor.models;
 
 public class InforSlots {
+	private InforUser inforUser;
+	private InforParking inforParking;
 	public InforSlots(){}
+	public InforUser getInforUser() {
+		return inforUser;
+	}
+	public void setInforUser(InforUser inforUser) {
+		this.inforUser = inforUser;
+	}
+	public InforParking getInforParking() {
+		return inforParking;
+	}
+	public void setInforParking(InforParking inforParking) {
+		this.inforParking = inforParking;
+	}
 }
